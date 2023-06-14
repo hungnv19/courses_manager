397bd13310b99245b0cc516f4faa842007d5139a
hungnp19072002 <hungnv19072002@gmail.com>
2023-06-14 10:25:05 +0700

    Create Database

---
+++ database/migrations/2023_06_14_032215_create_class_weekday_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('class_weekday', function (Blueprint $table) {
+           
+            $table->integer('class_id');
+            $table->integer('weekday_id');
+            $table->string('hours');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('class_weekday');
+    }
+};
