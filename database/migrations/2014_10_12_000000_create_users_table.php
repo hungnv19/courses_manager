56233c4b1a29d5f7c9f2acef7e17bddafe26f30b
hungnp19072002 <hungnv19072002@gmail.com>
2023-06-14 10:17:15 +0700

    Revert "Create Database"
    This reverts commit e10903a454238a1174911f0ad81abb1d06448d59.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,19 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('position');
-            $table->string('phone')->nullable();
-            $table->string('address')->nullable();
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('image')->nullable();
-            $table->string('date_birth')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->tinyInteger('level')->default(0);
-            $table->tinyInteger('status')->default(0);
-            $table->tinyInteger('role')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
